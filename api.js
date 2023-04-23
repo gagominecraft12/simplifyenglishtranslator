ab7b108ae90aa5a3cf030c85887fc5b36c522fda
gagominecraft12 <gaeliangarza1@gmail.com>
2023-04-23 17:32:07 -0600

    a

--- api.js
+++ api.js
@@ -1,1 +1,8 @@
 // please dont ruin, i can only code that weird thing once you start
+
+const bingtranslate = require("bing-translate-api");
+
+function a() {
+    bingtranslate.lang.LANGS["zh-Hans"];
+    
+}
---
+++ apiQuery.ts
@@ -0,0 +1,1 @@
+const bingtranslate = require("bing-translate-api");
